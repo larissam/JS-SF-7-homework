c39059a9b518ba678198104291657c9b68fcd641
Ming Wang <ming.wang@spigit.com>
2017-05-14 18:57:00 -0700

    homework for class 04 - forEach

--- Ming-mwangui/hw_3/5-random-address-generator-bonus/app.js
+++ Ming-mwangui/hw_3/5-random-address-generator-bonus/app.js
@@ -8,3 +8,28 @@ Optional bonus work on conditionals and functions:
     
     34578 Dolphin Street, Wonka NY, 44506
 */
+
+var streetNumber = [3421, 5346, 867, 89, 4, 5543];
+var streetName = ['Bush', 'Pine', 'Ellis', '4th', '5th Ave', 'Mission'];
+var cityNam = ['San Francisco', 'San Jose', 'Pleasanton', 'Dublin', 'Los Angeles'];
+var stateName = ['CA', 'NY', 'AZ', 'TX', 'AK', 'GA'];
+var zipCode = ['94113', '10087', '34000', '29301', '57320', '34921'];
+var items = [streetNumber, streetName, cityNam, stateName, zipCode];
+
+window.onload = function generateAddress() {
+    var newArray = [];
+    items.forEach(function (element, index) {
+        var rand = element[Math.floor(Math.random() * element.length)];
+        newArray = newArray.concat(rand);
+    });
+    console.log(newArray[0] + " " + newArray[1] + " st, " + newArray[2] + " " + newArray[3] + ", " + newArray[4]);
+
+    /* forEach function coming from this idea:
+    var rand1 = streetNumber[Math.floor(Math.random() * streetNumber.length)];
+    var rand2 = streetName[Math.floor(Math.random() * streetName.length)];
+    var rand3 = cityNam[Math.floor(Math.random() * cityNam.length)];
+    var rand4 = stateName[Math.floor(Math.random() * stateName.length)];
+    var rand5 = zipCode[Math.floor(Math.random() * zipCode.length)];
+    console.log(rand1 + " " + rand2 + " street, " + rand3 + " " + rand4 + ", " + rand5);
+    */
+};
