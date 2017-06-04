d43ff4c421870ac793bbab365fccff4c00d66a75
mwangui <ming.wang@mindjet.com>
2017-06-03 20:10:53 -0700

    99 bottles in Jquery

---
+++ Ming-mwangui/HomeHowrk_submit_2/HW_7/Jquery-99-bottles/app.js
@@ -0,0 +1,39 @@
+//variable for strings
+var singular = " bottle";
+var plural = " bottles";
+var paragraph1 = ' of beer on the wall, ';
+var paragraph2 = ' of beer. Take one down and pass it around, ';
+var paragraph3 = ' of beer on the wall.';
+var paragraph4 = 'no more';
+var finalLine1 = 'No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, ';
+var finalLine2 = ' bottles of beer on the wall.';
+
+//variable for a new array to storage i, so I know the total leangth without hard code the number inside the code
+var totalNum = [];
+
+//variable for ul
+var $ul = $('<ul>');
+$('body').add($ul);
+
+//document.body.appendChild($ul);
+
+function printResult () {
+    for ( var i = 99; i > 0; i-- ) {
+            var msg1 = i + plural + paragraph1 + i + plural + paragraph2 + (i-1) + plural + paragraph3;
+            totalNum.push(i);
+        if (i == 1) {
+            var msg1 = i + singular + paragraph1 + i + singular + paragraph2 + paragraph4 + plural + paragraph3;
+        };
+        var $li = $("<li> msg1 </li>");
+        $('#result').add($li);
+        console.log('test', $li);
+        //$ul.add($li);
+    }
+        // var $finalContainer = document.createElement("li");
+        // var finalText = document.createTextNode(finalLine1 + totalNum.length + finalLine2);
+        // $finalContainer.append(finalText);
+        // $ul.add($finalContainer);
+};
+
+printResult();
+
