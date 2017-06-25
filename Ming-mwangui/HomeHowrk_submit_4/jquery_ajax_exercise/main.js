624559e5c38da705ca41d79ba9c79fa2781447da
Ming Wang <ming.wang@spigit.com>
2017-06-24 20:09:26 -0700

    updated

--- Ming-mwangui/HomeHowrk_submit_4/jquery_ajax_exercise/main.js
+++ Ming-mwangui/HomeHowrk_submit_4/jquery_ajax_exercise/main.js
@@ -1,73 +1,50 @@
-/*
-
- 1. Sign up for openweathermap.org and generate an API key.
- 2. User either $.ajax or $.get to pull weather current data .
- for San Francisco (hint: http://api.openweathermap.org/data/2.5/weather?q=...).
- 3. Print the temperature in the console. Note that the temperature will be in Kelvin.
- 4. Incorporate the following code into your app to log a Fahrenheit value for the temperature to the console
- (Fahrenheit temperature is stored in the degFInt variable):
- var degF = (temp - 273.15) * 1.8 + 32;
- var degFInt = Math.floor(degF);
- 5. Use DOM manipulation to add the returned temperature to the span element with the id "temp".
- 6. Sign up for developer.weatherunlocked.com and obtain your application ID and application key.
- 7. Repeat the above steps to obtain the current temperature from this data source, convert it to Fahrenheit,
- log it to the console, and then add it to the DOM (uncomment out the 3 lines indicated in index.html for this step,
- and add your temperature value to the span elmeent with the id "temp2").
- 8. Refactor your code as necessary to separate the code for DOM manipulation from the code for sending an HTTP request.
- BONUS: Implement a form that prompts users for a city and state and returns data for the location they specify
- (Uncomment out the existing form code in index.html as marked).
-
- */
-
-
 'use strict';
 
-//weatherunlocked
-var weatherUrl = "http://api.openweathermap.org/data/2.5/weather?zip=94102,us";
-var apiKey = "your key here";
-var url = weatherUrl;
-$.get(url + '&APPID=' + apiKey, function (respone) {
-    console.log(respone);
-    openWeatherMapData(respone);
-});
+//openweathermap
+var weatherUrl = "http://api.openweathermap.org/data/2.5/weather?";
+var target = "zip=94102,us";
+var apiKey = "your api key here";
+var weatherinSF = weatherUrl + target + '&APPID=' + apiKey;
 
-function openWeatherMapData(respone) {
-    var degF = (respone.main.temp - 273.15) * 1.8 + 32;
+//weatherunlocked
+var unLockedUrl = "http://api.weatherunlocked.com/api/current/us.94102?app_id=";
+var appId = "cb51ba88";
+var key = "your api key here";
+var unLockedWeatherSF = unLockedUrl + appId + '&app_key='+ key;
+
+//global function for re-use
+function makeRequest(url, callBackFunciton) {
+    $.get(url, function (respaon) {
+        callBackFunciton(respaon);
+    });
+}
+
+//display data method
+function openWeatherMapData(respaon) {
+    var degF = (respaon.main.temp - 273.15) * 1.8 + 32;
     var degFInt = Math.floor(degF);
     $('#temp').html(degFInt + '°С');
 }
-
-//openweathermap
-var api = "http://api.weatherunlocked.com/api/current/us.94102?app_id=";
-var appId = "cb51ba88";
-var key = "your key here";
-
-$.get(api+ appId + '&app_key='+ key, function (respone) {
-    console.log('respone', respone);
-    weatherUnLockedData(respone);
-});
 
 function weatherUnLockedData(respone) {
     var temp = Math.floor(respone.temp_f);
     $('#temp2').html(temp + '°С');
 }
 
+makeRequest(weatherinSF, openWeatherMapData);
+makeRequest(unLockedWeatherSF, weatherUnLockedData);
+
+// For user search function
 $('#getTemp').on('click', function (e) {
     e.preventDefault();
     var city = $('#city').val();
     var state = $('#state').val();
-
-    var mapUrl = 'http://api.openweathermap.org/data/2.5/weather?q=';
     var keyin = city+ ',' +state;
-    var c = '&APPID=' + apiKey;
 
     if (city != '' && state != '') {
+        makeRequest(weatherUrl + 'q=' + keyin + '&APPID=' + apiKey, inQuiryData);
 
-        $.get(mapUrl + keyin + '&APPID=' + apiKey, function (respone) {
-            console.log(' xx respone:', respone);
-            data(respone);
-        });
-        function data(respone) {
+        function inQuiryData(respone) {
             var degF = (respone.main.temp - 273.15) * 1.8 + 32;
             var degFInt = Math.floor(degF);
             $('#temp3').html(degFInt + '°С');
@@ -78,24 +55,8 @@ $('#getTemp').on('click', function (e) {
     } else {
         alert('Please enter a value City & State');
     }
+
+    //empty input fiels
+    $('#city').val('');
+    $('#state').val('');
 });
-
-
-//oakland
-
-//http://api.openweathermap.org/data/2.5/weather?zip=94102,us
-
-//http://openweathermap.org/data/2.5/find?callback=jQuery1910931498094633922_1497240630122&q=Pleasanton,us&type=like&sort=population&cnt=30&appid=b1b15e88fa797225412429c1c50c122a1&_=1497240630124
-//  http://openweathermap.org/data/2.5/find?callback=jQuery1910931498094633922_…=population&cnt=30&appid=b1b15e88fa797225412429c1c50c122a1&_=1497240630124
-
-
-// console.log('response', respone);
-// console.log('main', respone.main);
-// console.log('The temperature in Kelvin is: ', respone.main.temp);
-// console.log('0: ', respone.weather[0].main);
-
-//https://openweathermap.org/appid#use - how to setup key inside the url
-//https://developer.weatherunlocked.com - api/{LocalWeatherType}/{Location}?app_id={APP_ID}&app_key={APP_KEY}
-//http://api.weatherunlocked.com/api/current/51.50,-0.12?app_id={APP_ID}&app_key={APP_KEY}
-//http://api.weatherunlocked.com/api/forecast/51.50,-0.12?app_id={APP_ID}&app_key={APP_KEY}
-//var weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=San,Francisco";
